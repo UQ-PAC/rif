94e7c224ed58f75daeff35143fe5fc0d3d7065a2
Andrew Brown <92134285+andrewj-brown@users.noreply.github.com>
2025-09-04 13:11:35 +1000

    move examples up one folder

--- examples/create.sh
+++ examples/create.sh
@@ -1,3 +1,3 @@
-aarch64-linux-gnu-gcc -O2 -march=armv8-a+nopauth $1 -o $1.out
+aarch64-linux-gnu-gcc -O2 -march=armv8-a+nopauth $1.c -o $1.out
 ddisasm $1.out
-gtirb-semantics $1.out.gtirb $1.out.gts --json $1.out.json
+gtirb-semantics $1.gtirb $1.gts --json $1.json
